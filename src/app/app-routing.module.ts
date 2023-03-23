09ea0e2f2bc2d972ad0a90d4eb1d43b5490b9621
June Dougherty <zachharlandougherty@gmail.com>
2023-03-23 19:36:46 -0400

    comit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserLoginComponent } from './user-login/user-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: UserLoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
