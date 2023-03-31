eedcec1fcae5e1979344070354fda3350ccb87be
June Dougherty <zachharlandougherty@gmail.com>
2023-03-30 22:01:26 -0400

    in class changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FollowUserComponent } from './follow-user/follow-user.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { ViewUserComponent } from './view-user/view-user.component';
 
 const routes: Routes = [
   { path: 'user-profile', component: UserProfileComponent },
   { path: 'view-user', component: ViewUserComponent },
+  { path: 'follow-user', component: FollowUserComponent },
 ];
 
 @NgModule({
