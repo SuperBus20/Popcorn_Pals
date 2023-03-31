1ac16d11fad93dc7e034e693eb89295e2d979439
anhie92 <anh.n.mai92@gmail.com>
2023-03-31 16:14:24 -0400

    march 31
    removed some unneeded code, moved search to nav bar, added favorites in user page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
+import { IMovie, IShow, ISource } from './Interfaces/Media';
+import { ILoggedInUser } from './Interfaces/LoggedinUser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Popcorn_Pals';
+  loggedInUser: ILoggedInUser | null = null;
+  constructor(private api: ApiService) {}
+
+  ngOnInit(): void {
+    this.api.loggedInEvent.subscribe(
+      (x) => (this.loggedInUser = x as ILoggedInUser)
+    );
+  }
 }
