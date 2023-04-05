a5fa809ea1f157d865c3b96c214e278c8ff8df7d
anhie92 <anh.n.mai92@gmail.com>
2023-04-05 14:38:40 -0400

    added home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FollowUserComponent } from './follow-user/follow-user.component';
+import { HomeComponent } from './home/home.component';
 import { SearchUserComponent } from './search-user/search-user.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { ViewUserComponent } from './view-user/view-user.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'user-profile', component: UserProfileComponent },
   { path: 'view-user/:username', component: ViewUserComponent },
   { path:'view-user', component: ViewUserComponent},
